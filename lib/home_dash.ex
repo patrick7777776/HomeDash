bc27eae94e950272915c987afce93d644316e74a
Patrick <patrick.tschorn@oliverwyman.com>
2020-05-01 10:27:39 +0100

    startup/shutdown order

--- lib/home_dash.ex
+++ lib/home_dash.ex
@@ -1,12 +1,12 @@
 defmodule HomeDash do
   def start(_type, _args) do
     children = [
-      {Scenic, viewports: [Application.get_env(:home_dash, :viewport)]},
       {HomeDash.Data.Inverter, nil},
       {HomeDash.Data.InverterFetcher, nil},
-      {HomeDash.Data.WeatherIcons, nil},
       {HomeDash.Data.MetWeather, nil},
-      {HomeDash.Data.MetWeatherFetcher, nil}
+      {HomeDash.Data.MetWeatherFetcher, nil},
+      {Scenic, viewports: [Application.get_env(:home_dash, :viewport)]},
+      {HomeDash.Data.WeatherIcons, nil}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
